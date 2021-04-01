d76ca868f3d6ad7e1b571aa432b44985269f1db5
sukunming <sukunming@sandboxol.com>
2021-04-01 19:53:56 +0800

    摄像机

--- OpenGLProject/4.1.texture.fs
+++ OpenGLProject/4.1.texture.fs
@@ -1,15 +1,14 @@
 ﻿#version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
 in vec2 TexCoord;
 
-// texture sampler
+// texture samplers
 uniform sampler2D texture1;
 uniform sampler2D texture2;
-uniform float alpha;
 
 void main()
 {
-	FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(TexCoord.x, TexCoord.y)), alpha);
+	// linearly interpolate between both textures (80% container, 20% awesomeface)
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
